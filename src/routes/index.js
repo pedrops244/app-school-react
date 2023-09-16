983790093672e4ee075b160251a228cc31660b1c
Pedro Souza <pedrops244@hotmail.com>
2023-09-16 19:33:37 -0300

    cleaning the base project and creating routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import MyRoute from './MyRoute';
+
+import Aluno from '../pages/Aluno';
+import Alunos from '../pages/Alunos';
+import Fotos from '../pages/Fotos';
+import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
-  // toast.success('Eu uso um ID personalizado', {
-  //   toastId: 'sucessId',
-  // });
-  // toast.error('Eu uso um ID personalizado', {
-  //   toastId: 'errorId',
-  // });
   return (
     <Switch>
-      <MyRoute exact path='/' component={Login} isClosed />
+      <MyRoute exact path='/' component={Alunos} isClosed={false} />
+      <MyRoute exact path='/aluno/:id/edit' component={Aluno} isClosed />
+      <MyRoute exact path='/aluno/' component={Aluno} isClosed />
+      <MyRoute exact path='/fotos/:id' component={Fotos} isClosed />
+      <MyRoute exact path='/login/' component={Login} isClosed={false} />
+      <MyRoute exact path='/register/' component={Register} isClosed={false} />
+
       <MyRoute path='*' component={Page404} />
     </Switch>
   );
