6f079005bf917ff67a0dc64326e1a32795f29691
Pedro Souza <pedrops244@hotmail.com>
2023-09-19 22:01:43 -0300

    creating register route and sending to back-end

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 0,
     'no-console': 0,
     'default-param-last': 0,
+    'react/jsx-no-bind': 0,
   },
 };
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -28,7 +28,11 @@ export default createGlobalStyle`
     padding: 10px 20px;
     border-radius: 5px;
     font-weight: 700;
+    transition: all 300ms;
   }
+  button:hover {
+      filter: brightness(85%);
+    }
 
   a {
     text-decoration: none;
