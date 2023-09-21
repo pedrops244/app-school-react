42a34d21ec0b25a0133f8b72d75533508a916bc7
Pedro Souza <pedrops244@hotmail.com>
2023-09-21 19:12:56 -0300

    creating and using a loading component

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import isEmail from 'validator/lib/isEmail';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
 import * as actions from '../../store/modules/auth/actions';
+import Loading from '../../components/Loading';
 
 export default function Login(props) {
   const dispach = useDispatch();
 
   const prevPath = get(props, 'location.state.prevPath', '/');
+  const isLoading = useSelector((state) => state.auth.isLoading);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -35,6 +37,7 @@ export default function Login(props) {
   };
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>Login</h1>
 
       <Form onSubmit={handleSubmit}>
--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -14,10 +14,16 @@ export default function reducer(state = initialState, action) {
       newState.isLoggedIn = true;
       newState.token = action.payload.token;
       newState.user = action.payload.user;
+      newState.isLoading = false;
       return newState;
     }
     case types.LOGIN_FAILURE: {
       const newState = { ...initialState };
+      return newState;
+    }
+    case types.LOGIN_REQUEST: {
+      const newState = { ...state };
+      newState.isLoading = true;
       return newState;
     }
 
